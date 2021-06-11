b6d9e2ff632e9fac2a58b6941038958f50ab7c9c
saadegh <sadeqgolandam@gmail.com>
2021-06-11 21:55:12 +0430

    incomein models and some beautification

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Expenses
+from .models import Expenses, Income
 
 admin.site.register(Expenses)
 # Register your models here.
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -8,3 +8,14 @@ class Expenses(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '{}-{}'.format(self.date, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}-{}'.format(self.date, self.amount)
+        
